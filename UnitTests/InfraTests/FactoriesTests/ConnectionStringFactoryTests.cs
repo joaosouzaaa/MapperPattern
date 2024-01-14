cd083e0791f0f932f2202cee72c2d99305802685
joaosouzaaa <joaoasouza982@gmail.com>
2024-01-14 17:43:46 -0300

    Tests: Infra tests

---
+++ UnitTests/InfraTests/FactoriesTests/ConnectionStringFactoryTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using Infra.Factories;
+
+namespace UnitTests.InfraTests.FactoriesTests;
+public sealed class ConnectionStringFactoryTests
+{
+    [Fact]
+    public void GetConnectionString_ReturnsContainerConnection()
+    {
+        // A
+        const string containerConnection = "test";
+        var inMemoryCollection = new Dictionary<string, string>()
+        {
+            {"ConnectionStrings:ContainerConnection", containerConnection }
+        };
+        IConfiguration configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(inMemoryCollection!)
+            .Build();
+
+        Environment.SetEnvironmentVariable("DOCKER_ENVIROMENT", "DEV_DOCKER_MAPPER");
+
+        // A
+        var connectionStringResult = configuration.GetConnectionString();
+
+        // A
+        Assert.Equal(containerConnection, connectionStringResult);
+    }
+
+    [Fact]
+    public void GetConnectionString_ReturnsLocalConnection()
+    {
+        // A
+        const string localConnection = "joao";
+        var inMemoryCollection = new Dictionary<string, string>()
+        {
+            {"ConnectionStrings:LocalConnection", localConnection}
+        };
+        IConfiguration configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(inMemoryCollection!)
+            .Build();
+
+        Environment.SetEnvironmentVariable("DOCKER_ENVIROMENT", "any value");
+
+        // A
+        var connectionStringResult = configuration.GetConnectionString();
+
+        // A
+        Assert.Equal(localConnection, connectionStringResult);
+    }
+}
